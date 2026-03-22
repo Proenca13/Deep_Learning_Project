86624d514ac6ffd15580943d08e072ebbf3c1cb1
João Proença <henriqueproenca2004@hotmail.com>
2026-03-22 22:35:07 +0000

    xception and plots for every model

--- models/classifiers/CNN/Xception.py
+++ models/classifiers/CNN/Xception.py
@@ -6,14 +6,12 @@ class DeepFakeXception(nn.Module):
     def __init__(self, freeze_features=True):
         super(DeepFakeXception, self).__init__()
 
-        self.xception = timm.create_model('xception', pretrained=True)
+        self.xception = timm.create_model('legacy_xception', pretrained=True)
 
-        # Freeze the core feature extraction layers
         if freeze_features:
             for param in self.xception.parameters():
                 param.requires_grad = False
 
-        # Replace the final layer for binary classification
         num_ftrs = self.xception.fc.in_features
         self.xception.fc = nn.Linear(num_ftrs, 1)
 
